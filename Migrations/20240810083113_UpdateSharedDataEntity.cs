74b992403a18ff1be8a547e3e735bde7f3f495ec
OniDem1 <spaindecraft17@gmail.com>
2024-08-10 15:58:30 +0700

    Update to Dev1.2.3

---
+++ Migrations/20240810083113_UpdateSharedDataEntity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WASA_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSharedDataEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OpenedShiftId",
+                table: "shared",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shared_Barcode",
+                table: "shared",
+                column: "Barcode")
+                .Annotation("Npgsql:NullsDistinct", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_shared_Barcode",
+                table: "shared");
+
+            migrationBuilder.DropColumn(
+                name: "OpenedShiftId",
+                table: "shared");
+        }
+    }
+}
