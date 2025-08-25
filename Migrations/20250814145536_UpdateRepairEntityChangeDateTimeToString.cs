f26846312b8990638bc58319d8254c4d15be72b9
Александр Демидов <Spaindecraft17@gmail.com>
2025-08-25 11:24:54 +0700

    *

---
+++ Migrations/20250814145536_UpdateRepairEntityChangeDateTimeToString.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WASA_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRepairEntityChangeDateTimeToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "RepairStatuses",
+                table: "repairs",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(int[]),
+                oldType: "integer[]");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IssueAt",
+                table: "repairs",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GetObjectAt",
+                table: "repairs",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreationAt",
+                table: "repairs",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContractSignAt",
+                table: "repairs",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int[]>(
+                name: "RepairStatuses",
+                table: "repairs",
+                type: "integer[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "IssueAt",
+                table: "repairs",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "GetObjectAt",
+                table: "repairs",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationAt",
+                table: "repairs",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ContractSignAt",
+                table: "repairs",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
